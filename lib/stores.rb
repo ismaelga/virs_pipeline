afd8077e5c994572887863063491ae8cefc3708a
Ismael Abreu <ismaelga@users.noreply.github.com>
2023-08-18 12:10:11 +0100

    Add inspections to store

--- lib/stores.rb
+++ lib/stores.rb
@@ -28,14 +28,52 @@ class SqliteStore
   end
 
   def get_org_name(id)
-    db.query("SELECT id, name FROM organizations WHERE id=? LIMIT 1", [id]).next
+    db.query("SELECT * FROM organizations WHERE id=? LIMIT 1", [id]).next
+  end
+
+  def upsert_inspection(vehicle_id:, inspection_date:, organization_id:, inspection_period_id:, inspection_passed:)
+    db.execute(
+      "
+      INSERT INTO vehicle_inspections(
+        vehicle_id, inspection_date, organization_id, inspection_period_id, inspection_passed
+      )
+      VALUES(?, ?, ?, ?, ?)
+      ON CONFLICT(vehicle_id, inspection_date)
+      DO UPDATE SET
+        organization_id=excluded.organization_id,
+        inspection_passed=excluded.inspection_passed
+      ",
+      [
+        vehicle_id,
+        inspection_date.to_s,
+        organization_id,
+        inspection_period_id,
+        inspection_passed.nil? ? nil : (inspection_passed ? 1 : 0)
+      ]
+    )
+  end
+
+  def get_inspection_by_vehicle_and_date(id, date)
+    r = db.query("SELECT * FROM vehicle_inspections WHERE vehicle_id=? AND inspection_date=? LIMIT 1", [id, date.to_s]).next
+    parse_inspection(r)
+  end
+
+  def parse_inspection(r)
+    r["inspection_passed"] = r["inspection_passed"].nil? ? nil : !!r["inspection_passed"]
+    r
   end
 
   def setup
     db.results_as_hash = true
 
     db.execute("CREATE TABLE IF NOT EXISTS organizations(id INT, name TEXT)")
-    db.execute("CREATE UNIQUE INDEX IF NOT EXISTS organizations_id_index ON organizations(id);")
+    db.execute("CREATE UNIQUE INDEX IF NOT EXISTS organizations_id_idx ON organizations(id);")
 
+    db.execute(
+      "CREATE TABLE IF NOT EXISTS vehicle_inspections(vehicle_id INT, inspection_date DATE, organization_id INT, inspection_period_id INT, inspection_passed BOOLEAN)"
+    )
+    db.execute("CREATE UNIQUE INDEX IF NOT EXISTS inspection_idx ON vehicle_inspections(vehicle_id, inspection_date);")
+    db.execute("CREATE INDEX IF NOT EXISTS vehicle_org_id_idx ON vehicle_inspections(organization_id);")
+    db.execute("CREATE INDEX IF NOT EXISTS vehicle_id_idx ON vehicle_inspections(vehicle_id);")
   end
 end
--- test/store_test.rb
+++ test/store_test.rb
@@ -31,4 +31,54 @@ describe SqliteStore do
     end
   end
 
+  describe "upsert_inspection" do
+    it "inserts a new inspection" do
+      @store.upsert_org(1, "Org A")
+
+      date = Date.parse("2020-01-01")
+
+      @store.upsert_inspection(
+        vehicle_id: 1,
+        inspection_date: date,
+        organization_id: 1,
+        inspection_period_id: 1,
+        inspection_passed: nil,
+      )
+
+      vehicle = @store.get_inspection_by_vehicle_and_date(1, date)
+      _(vehicle["vehicle_id"]).must_equal(1)
+      _(vehicle["inspection_date"]).must_equal("2020-01-01")
+      _(vehicle["organization_id"]).must_equal(1)
+      _(vehicle["inspection_period_id"]).must_equal(1)
+      _(vehicle["inspection_passed"]).must_equal(nil)
+    end
+
+    it "updates a inspection" do
+      @store.upsert_org(1, "Org A")
+
+      date = Date.parse("2020-01-01")
+      @store.upsert_inspection(
+        vehicle_id: 1,
+        inspection_date: date,
+        organization_id: 1,
+        inspection_period_id: 1,
+        inspection_passed: nil,
+      )
+
+      @store.upsert_inspection(
+        vehicle_id: 1,
+        inspection_date: date,
+        organization_id: 2,
+        inspection_period_id: 1,
+        inspection_passed: true,
+      )
+
+      vehicle = @store.get_inspection_by_vehicle_and_date(1, date)
+      _(vehicle["vehicle_id"]).must_equal(1)
+      _(vehicle["inspection_date"]).must_equal("2020-01-01")
+      _(vehicle["organization_id"]).must_equal(2)
+      _(vehicle["inspection_period_id"]).must_equal(1)
+      _(vehicle["inspection_passed"]).must_equal(true)
+    end
+  end
 end
